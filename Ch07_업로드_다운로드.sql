2dbb49018488efebc2559635460328a977ad4e6e
kanghyunuk <hukang.dev@gmail.com>
2026-04-09 16:37:27 +0900

    SQL

--- Ch07_업로드_다운로드.sql
+++ Ch07_업로드_다운로드.sql
@@ -12,8 +12,7 @@ delete from tbl_file;
 select * from tbl_file;
 
 insert into tbl_file
-values('practice.zip',load_file('c:\\sql\\practice.zip'));
-
+values('practice.zip',load_file('c:/sql/Microsoft Photos Installer.exe'));
 
 insert into tbl_file
 values('practice',load_file('c:\\sql\\a.pdf'));
@@ -21,8 +20,10 @@ insert into tbl_file
 values('practice3',load_file('c:\\sql\\ab.png'));
 insert into tbl_file
 values('practice4',load_file('c:\\sql\\VSCodeUserSetup-x64-1.85.1.exe'));
+
+
 use testdb;
 select * from tbl_file;
-
+-- 다운로드
 select filedata from tbl_file where title='practice.zip'
 into dumpfile 'c:/sql/down4.zip';
